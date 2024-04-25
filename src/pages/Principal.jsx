5687e2009681249212193d531b6c0a1b5ec3b47a
Pedro Henrique <pedrinhoavare18@gmail.com>
2024-04-25 07:57:33 -0300

    mais uma aula

--- src/pages/Principal.jsx
+++ src/pages/Principal.jsx
@@ -9,11 +9,11 @@ import jessica from '/Jessica Pearson.jpg'
 
 export default function Principal() {
   return (
-    <section className='flex flex-col items-center justify-center'>
+    <section className='flex flex-col items-center justify-center mx-36'>
         <Header/>
-        <div>
-            <h2 className='font-bold text-3xl'>Sinopse</h2>
-            <p>
+        <div className='flex flex-col items-center justify-center my-5'>
+            <h2 className='font-bold text-3xl my-5'>Sinopse</h2>
+            <p >
                 "Suits" é uma série de televisão americana criada por Aaron Korsh. A série estreou originalmente em 2011 no canal USA Network e ficou disponível para transmissão na Netflix em vários países.
 
                 A trama de "Suits" gira em torno de um brilhante estudante de direito, Mike Ross (interpretado por Patrick J. Adams), que, apesar de não ter frequentado a faculdade de direito, impressiona o renomado advogado Harvey Specter (interpretado por Gabriel Macht) durante uma entrevista de emprego. Harvey contrata Mike como seu novo associado, mesmo sabendo que ele não possui diploma de direito, e os dois formam uma dupla dinâmica enquanto lidam com casos jurídicos complexos.
@@ -26,19 +26,21 @@ export default function Principal() {
             </p>
         </div>
 
-        <div className='my-7'>
-            <h2 className='font-bold text-3xl '>Informações da série</h2>
-            <p>
-                Temporada 1: 12 episódios
-                Temporada 2: 16 episódios
-                Temporada 3: 16 episódios
-                Temporada 4: 16 episódios
-                Temporada 5: 16 episódios
-                Temporada 6: 16 episódios
-                Temporada 7: 16 episódios
-                Temporada 8: 16 episódios
-                Temporada 9: 10 episódios
-            </p>
+        <div>
+        <h2 className='font-bold text-3xl my-5'>Informações da série</h2>
+        </div>
+        <div className='my-5 justify-center items-center'>
+            
+            <p className='mb-3'>Temporada 1: 12 episódios</p>
+            <p className='mb-3'>Temporada 2: 16 episódios</p>
+            <p className='mb-3'>Temporada 3: 16 episódios</p>
+            <p className='mb-3'>Temporada 4: 16 episódios</p>
+            <p className='mb-3'>Temporada 5: 16 episódios</p>
+            <p className='mb-3'>Temporada 6: 16 episódios</p>
+            <p className='mb-3'>Temporada 7: 16 episódios</p>
+            <p className='mb-3'>Temporada 8: 16 episódios</p>
+            <p className='mb-3'>Temporada 9: 10 episódios</p>        
+            
         </div>
 
         <div>
@@ -47,37 +49,39 @@ export default function Principal() {
 
         <div className='flex my-12'>
             
-            <div className='mr-4'>
+            <div className=''>
                 <h3 className='font-semibold '>Harvey Specter</h3>
                 <img className='h-80 w-60' src={harvey} alt="" />
                 <p className='w-60'>Harvey é um advogado brilhante e carismático que trabalha no escritório de advocacia Pearson Hardman (mais tarde Pearson Specter e Pearson Specter Litt). Ele é conhecido por sua confiança, astúcia e habilidade de ganhar casos difíceis.</p>
             </div>
-            <div >
+            <div className='mx-16'>
                 <h3 className='font-semibold'>Mike Ross</h3>
                 <img className='h-80 w-64' src={mike} alt="" />
                 <p className='w-64'>Mike é um jovem inteligente com uma memória fotográfica que começa a trabalhar como associado na empresa de Harvey, embora não tenha frequentado a faculdade de direito nem tenha diploma. Ele possui um passado turbulento e esconde seu segredo de seus colegas de trabalho.</p>
             </div>
-            <div >
+            <div className=''>
                 <h3 className='font-semibold'>Rachel Zane</h3>
-                <img className='h-80 w-80' src={rachel} alt="" />
-                <p className='w-80'>Rachel é inicialmente apresentada como uma paralegal na empresa Pearson Hardman (mais tarde Pearson Specter e Pearson Specter Litt), onde trabalha enquanto frequenta a faculdade de direito à noite. Ela é inteligente, ambiciosa e determinada a se tornar uma advogada. Rachel é conhecida por sua ética de trabalho impecável e sua habilidade de lidar com situações difíceis.
+                <img className='h-80 w-72' src={rachel} alt="" />
+                <p className='w-72'>Rachel é inicialmente apresentada como uma paralegal na empresa Pearson Hardman (mais tarde Pearson Specter e Pearson Specter Litt), onde trabalha enquanto frequenta a faculdade de direito à noite. Ela é inteligente, ambiciosa e determinada a se tornar uma advogada. Rachel é conhecida por sua ética de trabalho impecável e sua habilidade de lidar com situações difíceis.
                 </p>
             </div>
-            <div >
-                <h3 className='font-semibold'>Donna Paulsen</h3>
-                <img className='h-80 w-64' src={donna} alt="" />
-                <p className='w-64'>Donna é a assistente executiva de Harvey e é conhecida por sua inteligência, perspicácia e habilidades excepcionais de resolução de problemas. Ela desempenha um papel crucial no sucesso da empresa e é uma figura confiável para muitos dos personagens.</p>
-            </div>
-            <div >
-                <h3 className='font-semibold'>Louis Litt</h3>
-                <img className='h-80 w-72' src={louis} alt="" />
-                <p className='w-72'>Louis é um dos principais sócios da empresa e inicialmente é um rival de Harvey. Ele é extremamente competitivo e muitas vezes luta para se destacar. Apesar de suas peculiaridades e excentricidades, ele é um advogado talentoso.</p>
-            </div>
-            <div >
-                <h3 className='font-semibold'>Jessica Pearson</h3>
-                <img className='h-80 w-64' src={jessica} alt="" />
-                <p className='w-64'>Jessica é a sócia fundadora do escritório de advocacia Pearson Hardman e uma figura de autoridade respeitada na comunidade jurídica. Ela é inteligente, poderosa e determinada a manter a reputação e o sucesso da empresa.</p>
-            </div>
+        </div>
+        <div className='flex my-12'>
+            <div  className=''>
+                    <h3 className='font-semibold'>Donna Paulsen</h3>
+                    <img className='h-80 w-64' src={donna} alt="" />
+                    <p className='w-64'>Donna é a assistente executiva de Harvey e é conhecida por sua inteligência, perspicácia e habilidades excepcionais de resolução de problemas. Ela desempenha um papel crucial no sucesso da empresa e é uma figura confiável para muitos dos personagens.</p>
+                </div>
+                <div  className='mx-16'>
+                    <h3 className='font-semibold'>Louis Litt</h3>
+                    <img className='h-80 w-64' src={louis} alt="" />
+                    <p className='w-64'>Louis é um dos principais sócios da empresa e inicialmente é um rival de Harvey. Ele é extremamente competitivo e muitas vezes luta para se destacar. Apesar de suas peculiaridades e excentricidades, ele é um advogado talentoso.</p>
+                </div>
+                <div >
+                    <h3 className='font-semibold'>Jessica Pearson</h3>
+                    <img className='h-80 w-64' src={jessica} alt="" />
+                    <p className='w-64'>Jessica é a sócia fundadora do escritório de advocacia Pearson Hardman e uma figura de autoridade respeitada na comunidade jurídica. Ela é inteligente, poderosa e determinada a manter a reputação e o sucesso da empresa.</p>
+                </div>
         </div>
 
         <a className='text-blue-500' href="https://www.netflix.com/br/title/70195800">Assista a série</a>
